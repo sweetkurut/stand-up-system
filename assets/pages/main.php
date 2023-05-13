f2ba1f007ad88c49098de5a9b51382215f0e595f
Trixter__JDM <54733786+trixtertop@users.noreply.github.com>
2023-05-13 12:03:06 +0600

    add in project sass

---
+++ assets/pages/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Home page</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Вопрос 1
+    </div>
+    <div class="col">
+      Вопрос 2
+    </div>
+    <div class="col">
+      Вопрос 3
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
