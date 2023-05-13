1a8feba43edefa5d4d0629fd55b8fdcdbe79c284
sweetkurut <devxwc@gmail.com>
2023-05-13 13:31:18 +0600

    added auth-page

---
+++ assets/pages/auth.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../assets/css/style.css">
+  <title>Авторизация</title>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="container">
+      <h1 class="auth__title">Авторизация</h1>
+      <form class="form">
+        <div class="input-fields">
+          <div class="input-group">
+            <label class="label"><h3 class="label-title">Email</h3></label>
+            <input type="text" class="input" placeholder="Введите почту">
+          </div>
+          <div class="input-group">
+            <label class="label"><h3 class="label-title">Password</h3></label>
+            <input type="password" class="input" placeholder="Введите пароль">
+          </div>
+          <div class="input-btn">
+            <button type="submit" class="btn">Отправить</button>
+          </div>
+          <div class="input-btn__req">
+            <div class="blog1"><p class="req-text">Не зарегистрировались?</p></div>
+            <div class="blog2"><button type="submit" class="btn-req">Зарегистрироваться</button></div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+<script src="../js/app.js"></script>
+</body>
+</html>
