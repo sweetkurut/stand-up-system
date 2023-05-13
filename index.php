52b6f16b14abdc6cb325c52fcf23d666287cab98
sweetkurut <devxwc@gmail.com>
2023-05-13 11:17:57 +0600

    create a folder for dev

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
   <title>Stand-Up-System</title>
 </head>
 <body>
-  <h1>hello</h1>
+
+
+<script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
